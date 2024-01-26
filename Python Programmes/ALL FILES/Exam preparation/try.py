7f337e733e108e87e01d2bc450d77747be55745d
Hari-Hacks <hari2006shankar@gmail.com>
2024-01-26 14:03:37 +0530

    final

--- Python Programmes/ALL FILES/Exam preparation/try.py
+++ Python Programmes/ALL FILES/Exam preparation/try.py
@@ -1,19 +1,14 @@
-def find():
-    with open('country.txt','w') as f:
-        l=['Whose woods these are I think I know.','His house is in the village though;','He will not see me stopping here','To watch his woods fill up with snow.']
+def ShowP():
+    with open('IONS.txt','w') as f:
+        l=['Whether an atom will form a cation or an anion is based on its','position in the periodic table is not a presumption']
         for i in l:
             f.write(i+'\n')
-    with open('country.txt','r') as f:
+    with open('IONS.txt','r') as f:
         l=f.readlines()
-        cw=ch=0
         for i in l:
-            if i[0][0].lower()=='w':
-                cw+=1
-            elif i[0][0].lower()=='h':
-                ch+=1
-            else: continue
-        print('W or w:',cw)
-        print('H or h:',ch)
-find()
-
-
+            for j in i.split():
+                if (j.lower().startswith('p')) and (j.lower().endswith('ion')):
+                    print(j)
+                else:
+                    continue  
+ShowP()
